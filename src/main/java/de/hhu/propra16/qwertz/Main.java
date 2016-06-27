91e0ea39991f6eeecc9895623b7932cd32c4abbe
Jan <jenibor2011@gmail.com>
2016-06-27 14:43:10 +0200

    Erster Commit .gitignore-Datei und Gradle konfiguriert Main-Klasse zum Testen hinzugefügt

---
+++ src/main/java/de/hhu/propra16/qwertz/Main.java
@@ -0,0 +1,11 @@
+package de.hhu.propra16.qwertz;
+
+/**
+ * Testklasse, wird bald wieder entfernt.
+ */
+public final class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}

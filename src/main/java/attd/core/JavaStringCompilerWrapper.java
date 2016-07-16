08d821161cbaf14fb600e9d9e422cf0dc6994096
niuhd100 <nick.uhde@web.de>
2016-07-16 12:35:20 +0200

    richtige Klassenversion

--- src/main/java/attd/core/JavaStringCompilerWrapper.java
+++ src/main/java/attd/core/JavaStringCompilerWrapper.java
@@ -1,152 +1,125 @@
 package attd.core;
 
+import vk.core.api.*;
+
 import java.time.Duration;
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Arrays;
 import java.util.List;
-import java.util.function.Predicate;
 
-import vk.core.api.CompilationUnit;
-import vk.core.api.CompileError;
-import vk.core.api.CompilerFactory;
-import vk.core.api.CompilerResult;
-import vk.core.api.JavaStringCompiler;
-
+/**
+ * Created by student on 15/07/16.
+ */
 public class JavaStringCompilerWrapper {
 
+    public CustomCompilerResult getCustomCompilerResult(String code,String test){
 
-    public CustomTestResult getTestResult(String code, String test) {
-        CompilationUnit c = new CompilationUnit(classNameParser(code), code, false);
-        CompilationUnit d = new CompilationUnit(classNameParser(test), test, true);
 
-        JavaStringCompiler compiler = CompilerFactory.getCompiler(c, d);
+        CompilationUnit a = new CompilationUnit(classNameParser(code), code,false);
+        CompilationUnit b = new CompilationUnit(classNameParser(test), test,true);
+
+        JavaStringCompiler javaStringCompiler = CompilerFactory.getCompiler(a, b);
 
         try{
+            javaStringCompiler.compileAndRunTests();
 
-            compiler.compileAndRunTests();
+            CustomErrorResult tests = checkCompilable(test,javaStringCompiler.getCompilerResult().getCompileDuration());
 
-            if (!compiler.getCompilerResult().hasCompileErrors()) {
-                return new CustomTestResult(true, compiler.getTestResult());
+            if(tests.hasCompileErrors()){
+                return new CustomCompilerResult(tests);
             }
+            if(javaStringCompiler.getCompilerResult().hasCompileErrors()){
+                return new CustomCompilerResult(new CustomErrorResult(javaStringCompiler.getCompilerResult().getCompilerErrorsForCompilationUnit(a), javaStringCompiler.getCompilerResult().getCompilerErrorsForCompilationUnit(b)));
+            }
+            return new CustomCompilerResult(javaStringCompiler.getTestResult());
+
         }catch (Exception e){
-            
+            return new CustomCompilerResult(new CustomErrorResult(Arrays.asList(new CompileError() {
+                @Override
+                public long getLineNumber() {
+                    return 0;
+                }
+
+                @Override
+                public long getColumnNumber() {
+                    return 0;
+                }
+
+                @Override
+                public String getMessage() {
+                    return e.getMessage();
+                }
+
+                @Override
+                public String getCodeLineContainingTheError() {
+                    String string="";
+                    for(StackTraceElement s : e.getStackTrace()){
+                        string+=s.toString();
+                    }
+                    return string;
+                }
+            })));
+
         }
-        return new CustomTestResult(false,null);
     }
 
 
-    public CompilerResult getCompilerResult(String test){
-        CompilationUnit d = new CompilationUnit(classNameParser(test), test, true);
 
-        JavaStringCompiler compiler = CompilerFactory.getCompiler(d);
-        List<CompileError> errors = new ArrayList<>();
-        List<CompileError> filtered = new ArrayList<>();
+    private CustomErrorResult checkCompilable(String test,Duration duration){
+        List<CompileError> compileErrors = new ArrayList<>();
+        if(!test.contains("@Test")){
+            compileErrors.add(new CompileError() {
+                @Override
+                public long getLineNumber() {
+                    return 0;
+                }
 
-                	
-					if (classNameParser(test)!=null) {
-					    compiler.compileAndRunTests();
-					    errors.addAll(compiler.getCompilerResult().getCompilerErrorsForCompilationUnit(d));
-					    compiler.getCompilerResult().getCompilerErrorsForCompilationUnit(d).stream().filter(predicate()).forEach(k -> filtered.add(k));
+                @Override
+                public long getColumnNumber() {
+                    return 0;
+                }
 
+                @Override
+                public String getMessage() {
+                    return "Die Klasse enthält keine @Test Annotation";
+                }
 
-					    if(test.contains("@Test") && test.contains("assert") && filtered.size()==0){
+                @Override
+                public String getCodeLineContainingTheError() {
+                    return "";
+                }
+            });
+        }
+        if(!test.contains("assert")){
+            compileErrors.add(new CompileError() {
+                @Override
+                public long getLineNumber() {
+                    return 0;
+                }
 
-					        return new CompilerResult() {
-					            @Override
-					            public boolean hasCompileErrors() {
-					                return false;
-					            }
+                @Override
+                public long getColumnNumber() {
+                    return 0;
+                }
 
-					            @Override
-					            public Duration getCompileDuration() {
-					                return compiler.getCompilerResult().getCompileDuration();
-					            }
+                @Override
+                public String getMessage() {
+                    return "Die Klasse enthält keine assert-Methode";
+                }
 
-					            @Override
-					            public Collection<CompileError> getCompilerErrorsForCompilationUnit(CompilationUnit cu) {
-					                return compiler.getCompilerResult().getCompilerErrorsForCompilationUnit(d);
-					            }
-					        };
-					    }else{
-							return new CompilerResult() {
-								@Override
-								public boolean hasCompileErrors() {
-									return true;
-								}
+                @Override
+                public String getCodeLineContainingTheError() {
+                    return "";
+                }
+            });
 
-								@Override
-								public Duration getCompileDuration() {
-									return compiler.getCompilerResult().getCompileDuration();
-								}
+        }
+        return new CustomErrorResult(compileErrors);
 
-								@Override
-								public Collection<CompileError> getCompilerErrorsForCompilationUnit(CompilationUnit cu) {
-									return filtered;
-								}
-							};
-						}
-
-					}
-
-        return new CompilerResult() {
-			
-			@Override
-			public boolean hasCompileErrors() {
-				// TODO Auto-generated method stub
-				return true;
-			}
-		
-			
-			@Override
-			public Collection<CompileError> getCompilerErrorsForCompilationUnit(CompilationUnit cu) {
-				List<CompileError>tCompileErrors  = new ArrayList<>();
-				tCompileErrors.add(new CompileError() {
-					
-					@Override
-					public String getMessage() {
-						// TODO Auto-generated method stub
-						return "Der Klassenname wurde nicht gefunden";
-					}
-					
-					@Override
-					public long getLineNumber() {
-						// TODO Auto-generated method stub
-						return 0;
-					}
-					
-					@Override
-					public long getColumnNumber() {
-						// TODO Auto-generated method stub
-						return 0;
-					}
-					
-					@Override
-					public String getCodeLineContainingTheError() {
-						// TODO Auto-generated method stub
-						return "";
-					}
-				});
-				return tCompileErrors;
-			}
-			
-			@Override
-			public Duration getCompileDuration() {
-				// TODO Auto-generated method stub
-				return Duration.ZERO;
-			}
-		};
     }
 
 
-    private Predicate<? super CompileError> predicate() {
-        return new Predicate<CompileError>() {
-            @Override
-            public boolean test(CompileError compileError) {
-                String e = compileError.getMessage();
-                return !(e.contains("cannot find symbol") && (e.contains("class") || e.contains("variable") || e.contains("method")));
-            }
-        };
-    }
+
 
     private String classNameParser(String code) {
 
@@ -155,7 +128,8 @@ public class JavaStringCompilerWrapper {
             return code.subSequence(code.indexOf("class") + 6, code.indexOf("{")).toString().replace(" ", "");
         } catch (StringIndexOutOfBoundsException e) {
 
-            return null;
+            return "";
         }
     }
+
 }
